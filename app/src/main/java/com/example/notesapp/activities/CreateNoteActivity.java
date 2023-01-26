fba3865fcbc10b1dc14968744c2a4bd3c755f0f5
l_keey14 <Leon062007@mail.ru>
2023-01-26 21:06:41 +0300

    Create database, note class

--- app/src/main/java/com/example/notesapp/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/notesapp/activities/CreateNoteActivity.java
@@ -1,4 +1,4 @@
-package com.example.notesapp;
+package com.example.notesapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.example.notesapp.R;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
